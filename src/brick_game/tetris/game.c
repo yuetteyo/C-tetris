6db32687ced4177ec65a62363bb5a24a6ee1c371
Yuette Yoko <yuetteyo@student.21-school.ru>
2024-06-01 22:45:16 +0300

    feat(front): added printNext func

--- src/brick_game/tetris/game.c
+++ src/brick_game/tetris/game.c
@@ -1,2 +1,9 @@
 #include "game.h"
 
+int **allocateMatrix(int rows, int cols) {
+  int **matrix = (int **)calloc(rows, sizeof(int *));
+  for (int i = 0; i < rows; ++i) {
+    matrix[i] = (int *)calloc(cols, sizeof(int));
+  }
+  return matrix;
+}
--- src/gui/cli/input.h
+++
@@ -1,9 +0,0 @@
-#ifndef INPUT_H
-#define INPUT_H
-
-#include <ncurses.h>
-#include "../../brick_game/tetris/game.h"
-
-UserAction_t getUserInput();
-
-#endif
